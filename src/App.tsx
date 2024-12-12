26f27d6df64ab931b5539c57c323a03aed3ea677
Sahib.Aujla <sahibpreet568@gmail.com>
2024-12-12 07:39:00 -0500

    added buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import * as faceapi from "face-api.js";
+import { useEffect } from "react";
+import { Container } from "react-bootstrap";
+import ControlBtns from "./components/ControlBtns";
 function App() {
+  useEffect(() => {
+    const loadModels = async () => {
+      const MODEL_URL = process.env.PUBLIC_URL + "/models";
+      await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+      await faceapi.nets.ageGenderNet.loadFromUri(MODEL_URL);
+      await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
+      await faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL);
+    };
+    loadModels();
+  }, []);
   return (
-    <>
-      <h1 className="text-4xl">Hello</h1>
-    </>
+    <Container fluid>
+      <h1 className="my-3">Face Recognition App</h1>
+      <div>
+        <ControlBtns />
+      </div>
+    </Container>
   );
 }
 
--- src/components/ControlBtns.tsx
+++ src/components/ControlBtns.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState, useAppDispatch } from "../store";
+import { startWebCam, stopWebCam } from "../store/webcamSlice";
+import { Button } from "react-bootstrap";
 const ControlBtns = () => {
-  return (
-    <div>ControlBtns</div>
-  )
-}
+  const dispatch = useAppDispatch();
+  const isOn = useSelector((state: RootState) => state.webcamReducer.isOn);
 
-export default ControlBtns
+  return (
+    <div className="mb-3">
+      {isOn ? (
+        <Button variant={"danger"} onClick={() => dispatch(stopWebCam())}>
+          Stop Video
+        </Button>
+      ) : (
+        <Button variant="success" onClick={() => dispatch(startWebCam())}>
+          Start Video
+        </Button>
+      )}
+    </div>
+  );
+};
+
+export default ControlBtns;
